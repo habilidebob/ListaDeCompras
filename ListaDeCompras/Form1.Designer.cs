d7ff1126def2fd9293a6aba9e3b714a444d1fbcc
Habilidebob <80491104+habilidebob@users.noreply.github.com>
2025-08-14 22:05:23 -0300

    Desenvolvimento da interface principal
    Criada a interface principal, com o logotipo, o título, os botões e a lista com as compras.

--- ListaDeCompras/Form1.Designer.cs
+++ ListaDeCompras/Form1.Designer.cs
@@ -28,13 +28,128 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pibLogo = new System.Windows.Forms.PictureBox();
+            this.lblTitulo = new System.Windows.Forms.Label();
+            this.lblNomeProduto = new System.Windows.Forms.Label();
+            this.txbProduto = new System.Windows.Forms.TextBox();
+            this.btnAdicionar = new System.Windows.Forms.Button();
+            this.libCompras = new System.Windows.Forms.ListBox();
+            this.btnExcluir = new System.Windows.Forms.Button();
+            this.btnLimpar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pibLogo)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pibLogo
+            // 
+            this.pibLogo.Image = global::ListaDeCompras.Properties.Resources.logo;
+            this.pibLogo.Location = new System.Drawing.Point(135, 23);
+            this.pibLogo.Name = "pibLogo";
+            this.pibLogo.Size = new System.Drawing.Size(100, 86);
+            this.pibLogo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pibLogo.TabIndex = 0;
+            this.pibLogo.TabStop = false;
+            // 
+            // lblTitulo
+            // 
+            this.lblTitulo.AutoSize = true;
+            this.lblTitulo.Font = new System.Drawing.Font("Futura Bk BT", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitulo.Location = new System.Drawing.Point(251, 41);
+            this.lblTitulo.Name = "lblTitulo";
+            this.lblTitulo.Size = new System.Drawing.Size(303, 45);
+            this.lblTitulo.TabIndex = 1;
+            this.lblTitulo.Text = "Lista de Compras";
+            // 
+            // lblNomeProduto
+            // 
+            this.lblNomeProduto.AutoSize = true;
+            this.lblNomeProduto.Location = new System.Drawing.Point(72, 204);
+            this.lblNomeProduto.Name = "lblNomeProduto";
+            this.lblNomeProduto.Size = new System.Drawing.Size(111, 16);
+            this.lblNomeProduto.TabIndex = 2;
+            this.lblNomeProduto.Text = "Nome do Produto:";
+            // 
+            // txbProduto
+            // 
+            this.txbProduto.Location = new System.Drawing.Point(75, 232);
+            this.txbProduto.Name = "txbProduto";
+            this.txbProduto.Size = new System.Drawing.Size(215, 23);
+            this.txbProduto.TabIndex = 3;
+            // 
+            // btnAdicionar
+            // 
+            this.btnAdicionar.Location = new System.Drawing.Point(75, 271);
+            this.btnAdicionar.Name = "btnAdicionar";
+            this.btnAdicionar.Size = new System.Drawing.Size(215, 47);
+            this.btnAdicionar.TabIndex = 4;
+            this.btnAdicionar.Text = "Adicionar";
+            this.btnAdicionar.UseVisualStyleBackColor = true;
+            // 
+            // libCompras
+            // 
+            this.libCompras.FormattingEnabled = true;
+            this.libCompras.ItemHeight = 16;
+            this.libCompras.Location = new System.Drawing.Point(390, 166);
+            this.libCompras.Name = "libCompras";
+            this.libCompras.Size = new System.Drawing.Size(299, 228);
+            this.libCompras.TabIndex = 5;
+            // 
+            // btnExcluir
+            // 
+            this.btnExcluir.Location = new System.Drawing.Point(390, 401);
+            this.btnExcluir.Name = "btnExcluir";
+            this.btnExcluir.Size = new System.Drawing.Size(146, 39);
+            this.btnExcluir.TabIndex = 6;
+            this.btnExcluir.Text = "Excluir";
+            this.btnExcluir.UseVisualStyleBackColor = true;
+            // 
+            // btnLimpar
+            // 
+            this.btnLimpar.Location = new System.Drawing.Point(543, 401);
+            this.btnLimpar.Name = "btnLimpar";
+            this.btnLimpar.Size = new System.Drawing.Size(146, 39);
+            this.btnLimpar.TabIndex = 7;
+            this.btnLimpar.Text = "Limpar Lista";
+            this.btnLimpar.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(739, 489);
+            this.Controls.Add(this.btnLimpar);
+            this.Controls.Add(this.btnExcluir);
+            this.Controls.Add(this.libCompras);
+            this.Controls.Add(this.btnAdicionar);
+            this.Controls.Add(this.txbProduto);
+            this.Controls.Add(this.lblNomeProduto);
+            this.Controls.Add(this.lblTitulo);
+            this.Controls.Add(this.pibLogo);
+            this.Font = new System.Drawing.Font("Futura Bk BT", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = ":: Lista de Compras ::";
+            ((System.ComponentModel.ISupportInitialize)(this.pibLogo)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pibLogo;
+        private System.Windows.Forms.Label lblTitulo;
+        private System.Windows.Forms.Label lblNomeProduto;
+        private System.Windows.Forms.TextBox txbProduto;
+        private System.Windows.Forms.Button btnAdicionar;
+        private System.Windows.Forms.ListBox libCompras;
+        private System.Windows.Forms.Button btnExcluir;
+        private System.Windows.Forms.Button btnLimpar;
     }
 }
 
